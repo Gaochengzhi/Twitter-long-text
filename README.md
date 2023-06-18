0775424019def88127bbb3c16e1cbf80470ed08a
Chengzhi Gao <kounarushi@Chengzhis-MacBook-Pro.local>
2023-06-18 10:27:36 +0800

    Fix the png transparent issues and add support for 996icu project

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 # <img src="public/icons/icon_128.png" width="48" align="left"> Twitter Long Text Editor
 
 ![chorme-v113-yellow](./assets/chorme-v113-yellow.svg)![test-windows-blue](./assets/test-windows-blue.svg)![build-macos-blue](./assets/build-macos-blue.svg)
+[![996.icu](https://img.shields.io/badge/link-996.icu-red.svg)](https://996.icu)[![LICENSE](https://img.shields.io/badge/license-Anti%20996-blue.svg)](https://github.com/996icu/996.ICU/blob/master/LICENSE)
 
 **280 characters? We can do more!**
 
@@ -28,9 +29,31 @@ Twitter Long Text Editor is a powerful Chrome extension that convert the input l
 
 ## Contribution
 
-This project was bootstrapped with [Chrome Extension CLI](https://github.com/dutiyesh/chrome-extension-cli), For further develop, please read their docs
+* This project was bootstrapped with [Chrome Extension CLI](https://github.com/dutiyesh/chrome-extension-cli), For further develop, please read their docs
 
-Rich text editor is supported by [Quilljs](https://quilljs.com/) and text convertion with [http-to-image](https://github.com/bubkoo/html-to-image/)
+TLDR:
+
+```shell
+npm run watch
+```
+
+Then follow these instructions to see your extension:
+
+1. Open **chrome://extensions**
+
+2. Check the **Developer mode** checkbox
+
+3. Click on the **Load unpacked extension** button
+
+4. Select the folder **my-extension/build**
+
+   
+
+* Rich text editor is supported by [Quilljs](https://quilljs.com/) and text convertion with [http-to-image](https://github.com/bubkoo/html-to-image/)
+
+
+
+* **Current issue**: The poorly designed editor insertion mechanism (with multiple complex timers) sometimes generates two instances and sometimes none at all.
 
 ## Contact me
 
--- src/contentScript.js
+++ src/contentScript.js
@@ -80,14 +80,14 @@ function initQuillEditor(path) {
     let quill = new Quill('#quill-editor', quillConfig);
 
     let toolbar = document.querySelector('.ql-toolbar');
-    toolbar.addEventListener('click', function(event) {
+    toolbar.addEventListener('click', function (event) {
         event.stopPropagation();
     });
 
     // Make textarea initially not visible and stop event propagation on input.
     let textarea = document.querySelector('div.DraftEditor-root');
     textarea.style.display = 'none';
-    textarea.addEventListener('input', function(event) {
+    textarea.addEventListener('input', function (event) {
         event.stopPropagation();
     });
 
@@ -98,7 +98,7 @@ function initQuillEditor(path) {
     uploadButton.style.position = 'inline-block';
     toolbar.appendChild(uploadButton);
 
-    uploadButton.addEventListener('click', function() {
+    uploadButton.addEventListener('click', function () {
         uploadClickHandler(editorContainer, quill, path);
     });
 }
@@ -121,18 +121,18 @@ const quillConfig = {
 function uploadClickHandler(editorContainer, quill, path) {
     let htmlContent = editorContainer;
     toBlob(htmlContent)
-        .then(function(bblob) {
+        .then(function (bblob) {
             let file = new File([bblob], 'my-node.png', { type: 'image/png' });
             navigator.clipboard.write([
                 new ClipboardItem({
                     'image/png': file
                 })
-            ]).then(function() {
+            ]).then(function () {
                 successNotification();
                 clearQuillEditor(quill);
                 hideQuillEditor(path);
                 showPen = !showPen;
-            }).catch(function(error) {
+            }).catch(function (error) {
                 console.error("Error: ", error);
             });
         });
@@ -146,9 +146,9 @@ function successNotification() {
 
     // Animate the notification
     notification.classList.add('show');
-    setTimeout(function() {
+    setTimeout(function () {
         notification.classList.add('fadeout');
-        setTimeout(function() {
+        setTimeout(function () {
             // Remove the notification element
             document.body.removeChild(notification);
         }, 300);
@@ -164,7 +164,9 @@ function checkForToolbarDiv() {
     if (toolbarDiv) {
         insertIcon();
     }
-    else { setTimeout(checkForToolbarDiv, 100); }
+    else { // Only start a new initialization if one isn't already in progress
+        setTimeout(checkForToolbarDiv, 100);
+    }
 }
 
 
@@ -176,8 +178,8 @@ setTimeout(() => {
         console.log(count1);
         checkForToolbarDiv();
     }
-}, 1500);
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+}, 1000);
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     if (request.message === 'urlChanged') {
         if (request.initialLoad) {
             // never going here
